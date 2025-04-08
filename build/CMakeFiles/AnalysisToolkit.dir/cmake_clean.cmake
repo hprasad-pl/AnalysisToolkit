9fbc94234034e92df4fb1f65b8e9392bb6e9a677
hprasad1924 <342949@uwr.edu.pl>
2025-04-08 23:04:47 +0200

    redesigned src/main.cc

---
+++ build/CMakeFiles/AnalysisToolkit.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/Users/hprasad/AnalysisToolkit/bin/AnalysisToolkit"
+  "/Users/hprasad/AnalysisToolkit/bin/AnalysisToolkit.pdb"
+  "CMakeFiles/AnalysisToolkit.dir/src/Graph.cc.o"
+  "CMakeFiles/AnalysisToolkit.dir/src/Graph.cc.o.d"
+  "CMakeFiles/AnalysisToolkit.dir/src/Histogram.cc.o"
+  "CMakeFiles/AnalysisToolkit.dir/src/Histogram.cc.o.d"
+  "CMakeFiles/AnalysisToolkit.dir/src/main.cc.o"
+  "CMakeFiles/AnalysisToolkit.dir/src/main.cc.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AnalysisToolkit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
